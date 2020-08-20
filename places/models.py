f766c9abeed5d3e04fa615b3212b4bf6a57ce1c6
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2020-08-20 13:39:59 +0300

    Add db_index to position field models.py

--- places/models.py
+++ places/models.py
@@ -4,22 +4,22 @@ from tinymce.models import HTMLField
 
 class Excursion(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название экскурсии')
-    description_short = models.TextField(blank=True, null=True, verbose_name='Короткое описание')
-    description_long = HTMLField(blank=True, null=True, verbose_name='Длинное описание')
-    lat = models.FloatField(blank=True, null=True, verbose_name='Широта')
-    lon = models.FloatField(blank=True, null=True, verbose_name='Долгота')
+    short_description = models.TextField(blank=True, verbose_name='Короткое описание')
+    long_description = HTMLField(blank=True, verbose_name='Длинное описание')
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    excursion_title = models.ForeignKey(Excursion, on_delete=models.CASCADE, null=True, blank=True)
-    image = models.ImageField(upload_to='', blank=True, null=True, verbose_name='Название картинки')
-    position = models.PositiveIntegerField(default=0, verbose_name='Позиция')
+    excursion_title = models.ForeignKey(Excursion, on_delete=models.CASCADE, verbose_name='Название экскурсии')
+    image = models.ImageField(upload_to='', verbose_name='Название картинки')
+    position = models.PositiveIntegerField(default=0, verbose_name='Позиция', db_index=True)
 
     class Meta:
         ordering = ['position']
 
     def __str__(self):
-        return f'{self.position} {self.excursion_title}'
+        return self.excursion_title
